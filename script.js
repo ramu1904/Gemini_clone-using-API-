9144ecf60ebb9c6dc86632a68128966c80815ccb
Ramu R <164389043+ramu1904@users.noreply.github.com>
2024-10-06 17:02:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let userMessage = null;
 let isResponseGenerating = false;
 
 // API configuration
-const API_KEY = "AIzaSyAXiehPHTyd3qdtLeWIVjgr8cGkUAYW95I"; // Your API key here
+const API_KEY = "YOUR API KEY HERE"; // Your API key here
 const API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
 
 // Load theme and chat data from local storage on page load
